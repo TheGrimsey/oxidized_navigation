61622a4ee568a95e5771f75c0e7de68a7c0a5af2
Jan Hohenheim <jan@hohenheim.ch>
2025-04-10 00:27:16 +0200

    Implement rasterization of compounds

--- src/conversion.rs
+++ src/conversion.rs
@@ -20,6 +20,7 @@ pub enum ColliderType {
     Cylinder(Cylinder),
     Cone(Cone),
     Triangle(Triangle),
+    Compound(Vec<ColliderType>),
 }
 
 pub enum GeometryToConvert {
@@ -30,6 +31,47 @@ pub enum GeometryToConvert {
 pub(super) enum Triangles {
     Triangle([Vec3; 3]),
     TriMesh(Box<[Vec3]>, Box<[[u32; 3]]>),
+}
+
+impl Default for Triangles {
+    fn default() -> Self {
+        Self::TriMesh(Box::new([]), Box::new([]))
+    }
+}
+
+impl Triangles {
+    fn extend(self, other: Triangles) -> Self {
+        match (self, other) {
+            (Self::Triangle(a), Self::Triangle(b)) => Self::TriMesh(
+                Box::new([a[0], a[1], a[2], b[0], b[1], b[2]]),
+                Box::new([[0, 1, 2], [3, 4, 5]]),
+            ),
+            (Self::TriMesh(verts, tris), Self::Triangle(other_verts))
+            | (Self::Triangle(other_verts), Self::TriMesh(verts, tris)) => {
+                let mut verts = verts.into_vec();
+                let mut tris = tris.into_vec();
+
+                let next_index = verts.len() as u32;
+                tris.push([next_index, next_index + 1, next_index + 2]);
+
+                verts.extend(
+                    other_verts
+                        .iter()
+                        .map(|point| Vec3::new(point.x, point.y, point.z)),
+                );
+
+                Self::TriMesh(verts.into_boxed_slice(), tris.into_boxed_slice())
+            }
+            (Self::TriMesh(verts, tris), Self::TriMesh(other_verts, other_tris)) => {
+                let mut verts = verts.into_vec();
+                verts.extend(other_verts);
+                let mut tris = tris.into_vec();
+                tris.extend(other_tris);
+
+                Self::TriMesh(verts.into_boxed_slice(), tris.into_boxed_slice())
+            }
+        }
+    }
 }
 
 const SUBDIVISIONS: u32 = 5;
@@ -49,29 +91,7 @@ pub(super) fn convert_geometry_collections(
 
 pub(super) fn convert_geometry(geometry_to_convert: GeometryToConvert) -> Triangles {
     match geometry_to_convert {
-        GeometryToConvert::Collider(collider) => {
-            let (vertices, triangles) = match collider {
-                ColliderType::Cuboid(cuboid) => cuboid.to_trimesh(),
-                ColliderType::Ball(ball) => ball.to_trimesh(SUBDIVISIONS, SUBDIVISIONS),
-                ColliderType::Capsule(capsule) => capsule.to_trimesh(SUBDIVISIONS, SUBDIVISIONS),
-                ColliderType::Cylinder(cylinder) => cylinder.to_trimesh(SUBDIVISIONS),
-                ColliderType::Cone(cone) => cone.to_trimesh(SUBDIVISIONS),
-                ColliderType::Triangle(triangle) => {
-                    return Triangles::Triangle(
-                        triangle
-                            .vertices()
-                            .map(|point| Vec3::new(point.x, point.y, point.z)),
-                    );
-                }
-            };
-
-            let vertices = vertices
-                .iter()
-                .map(|point| Vec3::new(point.x, point.y, point.z))
-                .collect();
-
-            Triangles::TriMesh(vertices, triangles.into_boxed_slice())
-        }
+        GeometryToConvert::Collider(collider) => rasterize_collider(collider),
         GeometryToConvert::ParryTriMesh(vertices, triangles) => {
             let vertices = vertices
                 .iter()
@@ -81,4 +101,43 @@ pub(super) fn convert_geometry(geometry_to_convert: GeometryToConvert) -> Triang
             Triangles::TriMesh(vertices, triangles)
         }
     }
+}
+
+fn rasterize_collider(collider: ColliderType) -> Triangles {
+    let triangles = Triangles::default();
+    rasterize_collider_inner(collider, triangles)
+}
+
+fn rasterize_collider_inner(collider: ColliderType, memoized_triangles: Triangles) -> Triangles {
+    let (vertices, triangles) = match collider {
+        ColliderType::Cuboid(cuboid) => cuboid.to_trimesh(),
+        ColliderType::Ball(ball) => ball.to_trimesh(SUBDIVISIONS, SUBDIVISIONS),
+        ColliderType::Capsule(capsule) => capsule.to_trimesh(SUBDIVISIONS, SUBDIVISIONS),
+        ColliderType::Cylinder(cylinder) => cylinder.to_trimesh(SUBDIVISIONS),
+        ColliderType::Cone(cone) => cone.to_trimesh(SUBDIVISIONS),
+        ColliderType::Triangle(triangle) => {
+            let triangle = Triangles::Triangle(
+                triangle
+                    .vertices()
+                    .map(|point| Vec3::new(point.x, point.y, point.z)),
+            );
+
+            return memoized_triangles.extend(triangle);
+        }
+        ColliderType::Compound(colliders) => {
+            let mut memoized_triangles = memoized_triangles;
+            for collider in colliders {
+                memoized_triangles = rasterize_collider_inner(collider, memoized_triangles);
+            }
+            return memoized_triangles;
+        }
+    };
+
+    let vertices = vertices
+        .iter()
+        .map(|point| Vec3::new(point.x, point.y, point.z))
+        .collect();
+
+    let trimesh = Triangles::TriMesh(vertices, triangles.into_boxed_slice());
+    memoized_triangles.extend(trimesh)
 }
